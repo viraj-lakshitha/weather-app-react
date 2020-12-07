9448dad2243a3f19157c09f9d07ce8837369d637
vitiya99 <virajlakshithabandara@gmail.com>
2020-12-07 19:43:11 +0530

    Revert "Add Bootstrap Package and app_component"
    This reverts commit f1b70325f425b14459000001feb699a048bf8210.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-import Weather from './app_component/weather.component';
 
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
